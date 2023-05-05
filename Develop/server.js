a1bd001b34a97b1e0e3c2091d8297fd312718c34
dctristero <123331986+dctristero@users.noreply.github.com>
2023-05-04 22:43:12 -0500

    please

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const db = require('./db/db.json');
 const app = express();
 const route = require("./routes/routes")
